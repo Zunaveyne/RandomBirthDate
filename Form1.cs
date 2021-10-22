f6e3f480de91dedccc2e7c526999f3089b1c5ddf
Александр Щелочков <shchelochkov.alex@gmail.com>
2021-10-22 17:43:35 +0300

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RandomBirthDate
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void GetRandom_Click(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+            string BirthDate;
+            int age;
+            int currentYear = DateTime.Now.Year;
+            int dayBirth;
+            int monthBirth;
+
+            if (int.TryParse(AgeText.Text, out age) == false)
+             {
+                MessageBox.Show("Неправильно введен возраст", "Critical Warning", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+             }
+
+            if (age == 0) 
+            {
+                currentYear -= rand.Next(18, 60);
+                dayBirth = rand.Next(1, 30);
+                monthBirth = rand.Next(1, 12);
+                BirthDate = $"{ResNumber(dayBirth)}.{ResNumber(monthBirth)}.{currentYear}";
+                DateText.Text = BirthDate;
+            }
+            else if (age > 0)
+            {
+                dayBirth = rand.Next(1, 30);
+                monthBirth = rand.Next(1, 12);
+                BirthDate = $"{ResNumber(dayBirth)}.{ResNumber(monthBirth)}.{currentYear - age}";
+                DateText.Text = BirthDate;
+            }
+            else MessageBox.Show("Нужно больше 0", "Critical Warning", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        static string ResNumber(int number)
+        {
+            string resNumber;
+
+            if (number >= 1 && number <= 9) resNumber = "0" + number;
+            else resNumber = number.ToString();
+            return resNumber;
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
